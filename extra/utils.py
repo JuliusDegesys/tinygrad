eb1a229843716679181241a89b0b214815328963
Julius Degesys <Julius.Degesys@gmail.com>
2023-06-11 16:38:37 -0400

    Improve download_file error message
    From this:
      File "/src/tinygrad/extra/utils.py", line 38, in download_file
        assert r.status_code == 200
               ^^^^^^^^^^^^^^^^^^^^
    AssertionError
    
    To this:
      File "/src/tinygrad/extra/utils.py", line 37, in download_file
        r.raise_for_status()
      File "/src/tinygrad/.venv/lib/python3.11/site-packages/requests/models.py", line 1021, in raise_for_status
        raise HTTPError(http_error_msg, response=self)
    requests.exceptions.HTTPError: 404 Client Error: Not Found for url: https://raw.githubusercontent.com/geohot/tinygrad/master/docs/stable_diffusion_by_tinygrad.jpg

--- extra/utils.py
+++ extra/utils.py
@@ -34,7 +34,7 @@ def download_file(url, fp, skip_if_exists=True):
   if skip_if_exists and os.path.isfile(fp) and os.stat(fp).st_size > 0:
     return
   r = requests.get(url, stream=True)
-  assert r.status_code == 200
+  r.raise_for_status()
   progress_bar = tqdm(total=int(r.headers.get('content-length', 0)), unit='B', unit_scale=True, desc=url)
   with tempfile.NamedTemporaryFile(dir=pathlib.Path(fp).parent, delete=False) as f:
     for chunk in r.iter_content(chunk_size=16384):
